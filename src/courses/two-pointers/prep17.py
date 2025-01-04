c3c46edcfff1380c9702a95da6c91554d3f7e49c
Alexander Zhipa <alex.zhipa@proton.me>
2025-01-03 21:25:20 -0500

    https://www.codechef.com/practice/course/two-pointers/POINTERF/problems/PREP17

---
+++ src/courses/two-pointers/prep17.py
@@ -0,0 +1,7 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = (int(i) for i in input().split())
+    b = (int(i) for i in input().split())
+    res = len(set(a) & set(b))
+    print(res)
